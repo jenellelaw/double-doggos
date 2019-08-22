266846308db4a91cb94fdfc95e42debf638cce33
Jenelle <lawjenelle@gmail.com>
2019-08-22 15:18:18 -0400

    pairs can be matched and the user can win. :raised_hands:

--- scripts/game.js
+++ scripts/game.js
@@ -1,64 +1,104 @@
-$(document).ready(function () {
-  // DOCUMENT READY STARTS HERE
+game = {
+  // PROPERTIES
+  remainingDogsToMatch: [], // has a duplicate of the DOM card elements
+  card1: '',
+  card2: '',
+  matchedBreed: '',
 
-  // Game namespace
-  const game = {
-    dogCount: [], // has a duplicate of the DOM card elements
-    renderCards() {
-      const dogs = ['corgi', 'beagle', 'frenchie', 'schnauzer', 'retriever', 'samoyed', 'poodle', 'rottweiler'];
+  // METHODS
+  renderCards() {
+    const dogs = ['corgi', 'beagle', 'frenchie', 'schnauzer', 'retriever', 'samoyed', 'poodle', 'rottweiler'];
 
-      this.dogCount = [...dogs]
+    this.remainingDogsToMatch = [...dogs]
 
-      const doubleDogs = [...dogs].concat([...dogs]);
+    const doubleDogs = [...dogs].concat([...dogs]);
 
-      // Shuffle doubleDogs - https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
-      for (let i = doubleDogs.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [doubleDogs[i], doubleDogs[j]] = [doubleDogs[j], doubleDogs[i]];
-      }
-
-      doubleDogs.forEach((dog) => {
-        const cardHTML = `<li class="card" tabindex="0" data-breed="${dog}"><div class="cardFront"> <img src="assets/dog-card-${dog}.svg" alt="${dog}"></div><div class="cardBack"></div></li>`;
-        $('#gameGrid').append(cardHTML);
-      });
-    },
-    populateCardArray() {
-      this.cardArray = $('.card').toArray();
-    },
-    // INIT METHOD
-    init() {
-      this.renderCards();
-      this.populateCardArray();
-      console.log(this.cardArray);
+    // Shuffle Dog Array
+    for (let i = doubleDogs.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [doubleDogs[i], doubleDogs[j]] = [doubleDogs[j], doubleDogs[i]];
     }
-  };
+
+    doubleDogs.forEach((dog) => {
+      const cardHTML = `<li class="card" tabindex="0" data-breed="${dog}"><div class="cardFront"> <img src="assets/dog-card-${dog}.svg" alt="${dog}"></div><div class="cardBack"></div></li>`;
+      $('#gameGrid').append(cardHTML);
+    });
+  },
+
+  // INIT METHOD
+  init() {
+    this.renderCards();
+  }
+};
+
+
+// DOCUMENT READY STARTS HERE
+
+$(document).ready(function () {
 
   game.init();
 
-  // General code goes here
+  // EVENT LISTENERS
 
-  $('#gameGrid').on('click', 'li', function () {
-    console.log("an item was clicked!");
+  // Event delegation for each card element because they are dynamically generated
+  $('#gameGrid').on('click', '.card', function () {
+    // Add card flip animation to show user that the card they clicked on has been selected
+    $(this).addClass('flip');
 
-    // when you select a card, remove the event listener from it (to prevent user from selecting it again)
+    // if there is no data saved in card1
+    if (!game.card1) {
+      game.card1 = $(this);
+      console.log('Card 1', game.card1, 'Card 2', game.card2);
+
+      // if there is data saved in card1, but no data in card2
+    } else if (game.card1 && !game.card2) {
+      game.card2 = $(this);
+      console.log('Card 1', game.card1, 'Card 2', game.card2);
+
+      // Check if the breed of card1 and card2 match
+      if (game.card1.data('breed') === game.card2.data('breed')) {
+        console.log('THE TWO CARDS ARE MATCHING')
+
+        // refactor this into a function that removes the breed from the list
+        game.matchedBreed = game.card1.data('breed');
+        const breedIndex = game.remainingDogsToMatch.indexOf(game.matchedBreed);
+        if (breedIndex > -1) { game.remainingDogsToMatch.splice(breedIndex, 1); }
+
+        console.log(game.remainingDogsToMatch);
+
+        if (game.remainingDogsToMatch.length === 0) {
+          alert('You win!');
+        }
+
+        // create a function to clear the saved cards
+        game.card1 = '';
+        game.card2 = '';
+
+      } else {
+        console.log('THE TWO CARDS DO NOT MATCH')
+
+        setTimeout(function () {
+          game.card1.removeClass('flip');
+          game.card2.removeClass('flip');
+          // create a function to clear the saved cards
+          game.card1 = '';
+          game.card2 = '';
+        }, 1000);
+      }
+    }
+
+
+    // disableCardSelect function
+
+
+
+
+
+
+    // When a card has been selected, remove the click listener from it, so it doesn't get selected again
+
 
     // if firstcard variable does not equal to null/nothing, then populate the second card
-
-
-    // if the data attribute of the first card matches the second card, then we have a match
-    // 
-
-    // const firstCard = $(this);
-    // const secondCard = $(this);
-
-    // console.log(firstCard);
-
-    // if firstCard === firstCard 
-    // then populate secondCard
-
-
-
-    // when item is clicked, add class of .selected
   });
 
 
