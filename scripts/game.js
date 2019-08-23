5efb32c8f6de5687f345af9de697ee4e52c65d99
Jenelle <lawjenelle@gmail.com>
2019-08-23 17:07:23 -0400

    Dog facts render to the screen

--- scripts/game.js
+++ scripts/game.js
@@ -4,7 +4,42 @@ game = {
   card1: '',
   card2: '',
   matchedBreed: '',
-
+  attempts: 0,
+  dogFacts: {
+    corgi: [
+      `Corgi means dwarf dog in welsh and were originally used as herders`,
+      `A Pembroke Welsh Corgi Named Rufus Was the Mascot for Amazon.`
+    ],
+    beagle: [
+      `Beagle Means "Loudmouth" in French, as they are known for being very vocal, with barking, baying and howling.`,
+      `The "Peanuts" character Snoopy is arguably the most famous fictitious Beagle. Snoopy was silent for the first few years of the comic strip, but later was given an active imagination and inner monologue.`
+    ],
+    frenchie: [
+      `Frenchies can't swim due to their smushed faces, thick neck structures and short bodies.They need to give a lot of forces just to keep their heads above the water.`,
+      `Frenchies aren’t not great on planes because their cute smushy - face causing breathing problems and cause their airways to collapse.`
+    ],
+    schnauzer: [
+      `Schnauzer comes from the German word for muzzle, referring to the dog’s square snout and distinct facial hair.`,
+      `Schnauzers are great all-around farm dogs and ratters which makes them fearless, but not aggressive.`
+    ],
+    retriever: [
+      `Golden Retrievers are considered to be the 4th smartest dog breed (in line behind Border Collies, Poodles, and German Shepherds).`,
+      `Golden Retrievers are known to become sad and even depressed when left alone for long periods of time. Experts say they shouldn't be left alone for more than seven hours!`,
+      `Golden Retrievers are often used on search and rescue teams because of their keen sense of smell and tracking abilities. be left alone for more than seven hours!`
+    ],
+    samoyed: [
+      `Samoyeds were ancient companions to Samoyed people who lived in Siberia. The working canines pulled sleds, hunted game, and herded reindeer.`,
+      `Dubbed the “Sammy smile,” Samoyeds have lips that naturally curve upwards and always wear a happy expression.`
+    ],
+    poodle: [
+      `Despite their French reputation, poodles hail from Germany, where they were called pudel, which is German for “puddle.`,
+      `Unlike dogs that shed, the poodle will grow fur continuously, and therefore need regular grooming. If left ungroomed, their fur will become matted and dreadlock-like.`
+    ],
+    rottweiler: [
+      `Rotties were herding dogs that almost became extinct in the 1800s due to industrialization, as their jobs became replaced by railroads which made it easier to transport herds.`,
+      `Rottweilers have a more impressive bite than German shepherds and pit bulls. At a bite force of 328 pounds — that’s about half of a shark’s bite force, at 669 pounds.`
+    ]
+  },
   // METHODS
   renderCards() {
     const dogs = ['corgi', 'beagle', 'frenchie', 'schnauzer', 'retriever', 'samoyed', 'poodle', 'rottweiler'];
@@ -26,103 +61,81 @@ game = {
       [array[i], array[j]] = [array[j], array[i]];
     }
   },
-  flipCard(card) {
-    card.addClass('flip');
-  },
   saveCard(selectedCard) {
     if (selectedCard[0] === game.card1[0]) return;
 
-    game.flipCard(selectedCard);
+    selectedCard.addClass('flip');
 
     if (!game.card1) {
       game.card1 = selectedCard;
-      // console.log('Card 1')
       return;
     }
 
-    game.lockGameBoard();
-
     game.card2 = selectedCard;
-    this.checkForMatch();
+    game.attempts++;
+    console.log(game.attempts);
 
-    // if (!game.card1) {
-    //   game.card1 = selectedCard;
-    //   console.log('Card 1', game.card1, 'Card 2 has no data');
-    // }
-    // else {
-
-    //   console.log('Card 1', game.card1, 'Card 2', game.card2);
-
-    //   this.checkForMatch();
-
-
-    // if (!game.card1) {
-    //   game.card1 = selectedCard;
-    //   console.log('Card 1', game.card1, 'Card 2 has no data');
-    // }
-    // else {
-    //   if (game.card1[0] === selectedCard[0]) return;
-    //   game.card2 = selectedCard;
-    //   console.log('Card 1', game.card1, 'Card 2', game.card2);
-
-    //   this.checkForMatch();
-
+    game.lockGameBoard();
+    game.checkCardsMatch();
   },
-  checkForMatch() {
-    // Check if the breed of card1 and card2 match
+  checkCardsMatch() {
     if (game.card1.data('breed') === game.card2.data('breed')) {
-      console.log('THE TWO CARDS ARE MATCHING');
-      game.card1.css("pointer-events", "none");
-      game.card2.css("pointer-events", "none");
-      this.removeBreedFromList();
-      this.clearSavedCardsData();
-      this.unlockGameBoard();
+      game.card1.css("pointer-events", "none").removeAttr('tabindex');
+      game.card2.css("pointer-events", "none").removeAttr('tabindex');
+      game.removeMatchedBreedFromList();
+      game.showMatchedBreedFact();
+      game.clearSavedCardsData();
+      game.unlockGameBoard();
     } else {
-      console.log('THE TWO CARDS DO NOT MATCH');
-
       setTimeout(function () {
-        console.log('setTimeout happens here');
         game.card1.removeClass('flip');
         game.card2.removeClass('flip');
 
         setTimeout(function () {
           game.clearSavedCardsData();
           game.unlockGameBoard();
-          console.log('END OF TRANSITION DETECTED');
-        }, 500);
+        }, 500); // 500ms is the amount of time needed for .removeClass('flip') transition to complete
 
-        // game.card1.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
-        //   game.clearSavedCardsData();
-        //   game.unlockGameBoard();
-        //   console.log('END OF TRANSITION DETECTED');
-        // });
-      }, 1500);
+      }, 1500); // let user look at not matching cards for 1500ms before removing the class
     }
   },
-  removeBreedFromList() {
+  removeMatchedBreedFromList() {
     game.matchedBreed = game.card1.data('breed');
-
-    console.log(game.matchedBreed);
 
     const breedIndex = game.remainingDogsToMatch.indexOf(game.matchedBreed);
     if (breedIndex > -1) { game.remainingDogsToMatch.splice(breedIndex, 1); }
 
-    console.log(game.remainingDogsToMatch);
-
     if (game.remainingDogsToMatch.length === 0) {
       alert('You win!');
     }
+  },
+  showMatchedBreedFact() {
+    console.log(game.matchedBreed);
+    const dogFactsArray = game.dogFacts[game.matchedBreed];
+    console.log(dogFactsArray);
+    const randomIndex = Math.floor(Math.random() * dogFactsArray.length);
+    console.log('random index', randomIndex);
+
+    const randomFact = dogFactsArray[randomIndex];
+    console.log(randomFact);
+
+    const factBreedName = `<h2 class="breedName">${game.matchedBreed}</h2>`;
+    const factText = `<p class="breedFact">${randomFact}</p>`;
+    const factImage = `<img src="" alt=""/>`;
+
+    $('#factCard').html('');
+    $('#factCard').append(factBreedName, factText, factImage);
   },
   clearSavedCardsData() {
     game.card1 = '';
     game.card2 = '';
   },
   lockGameBoard() {
-    console.log('Game board LOCKED.')
+    // console.log('Game board LOCKED.')
     $('#gameGrid').off('click keypress');
   },
   unlockGameBoard() {
-    console.log('Game board is UNLOCKED.');
+    // console.log('Game board is UNLOCKED.');
     $('#gameGrid').on('click keypress', '.card', function (e) {
       game.saveCard($(this));
     });
@@ -130,13 +143,13 @@ game = {
 
   // INIT METHOD
   init() {
-    this.renderCards();
-    this.unlockGameBoard();
+    game.renderCards();
+    game.unlockGameBoard();
   }
 };
 
-// DOCUMENT READY STARTS HERE
 
+// DOCUMENT READY STARTS HERE
 $(document).ready(function () {
   game.init();
   // DOCUMENT READY ENDS HERE
